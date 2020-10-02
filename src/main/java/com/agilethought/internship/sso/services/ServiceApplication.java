906bb24487de7e58cc4bbbeb8da21eb34b8d1496
Nemere <gamedann@hotmail.com>
2020-10-02 10:21:35 -0500

    Update ServiceApplication.java
    Add "import java.util.List;"

--- src/main/java/com/agilethought/internship/sso/services/ServiceApplication.java
+++ src/main/java/com/agilethought/internship/sso/services/ServiceApplication.java
@@ -3,9 +3,11 @@ package com.agilethought.internship.sso.services;
 import com.agilethought.internship.sso.model.User;
 import com.agilethought.internship.sso.model.UserId;
 
+import java.util.List;
+
 public interface ServiceApplication {
 
 	public UserId createUser(User user);
-	public java.util.List<User> getUsers();
+	public List<User> getUsers();
 
 }
