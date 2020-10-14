e9554ab7664f4ead5f9c7074f4d196572d1d419d
Luis Felipe Olivo <45859423+Bwore@users.noreply.github.com>
2020-10-14 08:47:53 -0500

    Indent

--- src/main/java/com/agilethought/internship/sso/swagger/SwaggerConfig.java
+++ src/main/java/com/agilethought/internship/sso/swagger/SwaggerConfig.java
@@ -11,14 +11,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.agilethought.internship.sso.controller"))
-				.paths(PathSelectors.any())
-				.build();
+		return new Docket(DocumentationType.SWAGGER_2).select()
+			.apis(RequestHandlerSelectors.basePackage("com.agilethought.internship.sso.controller"))
+			.paths(PathSelectors.any()).build();
 	}
-	
 }
