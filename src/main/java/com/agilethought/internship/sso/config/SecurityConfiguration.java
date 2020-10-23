36cedf2bfabc892ceb769d2fc1123698078ce13d
Jose Lazcano <Jose Lazcano@DESKTOP-5F1I1BJ>
2020-10-22 19:55:09 -0500

    Exposed Swagger endpoint + Log4j implementation

--- src/main/java/com/agilethought/internship/sso/config/SecurityConfiguration.java
+++ src/main/java/com/agilethought/internship/sso/config/SecurityConfiguration.java
@@ -14,7 +14,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/api/v1/user");
+		web.ignoring().antMatchers("/**");
 	}
 
 	@Bean
