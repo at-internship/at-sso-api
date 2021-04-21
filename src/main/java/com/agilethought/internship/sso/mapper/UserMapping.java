d03b6de04dd2d0dc7adc53d963ab59b3a6519031
graciela24 <mariagraciela_torres@ucol.mx>
2021-04-21 18:39:53 -0500

    Add class UserMapping

---
+++ src/main/java/com/agilethought/internship/sso/mapper/UserMapping.java
@@ -0,0 +1,18 @@
+package com.agilethought.internship.sso.mapper;
+
+import com.agilethought.internship.sso.domain.UpdateUserResponse;
+import org.springframework.stereotype.Component;
+
+import ma.glasnost.orika.MapperFactory;
+import net.rakugakibox.spring.boot.orika.OrikaMapperFactoryConfigurer;
+
+@Component
+public class UserMapping implements OrikaMapperFactoryConfigurer {
+
+	@Override
+	public void configure(MapperFactory orikaMapperFactory) {
+
+		orikaMapperFactory.classMap(com.agilethought.internship.sso.model.User.class, UpdateUserResponse.class).mapNulls(false).byDefault().register();
+	}
+
+}
