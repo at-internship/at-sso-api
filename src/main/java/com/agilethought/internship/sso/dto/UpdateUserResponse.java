dcc6122c822966ade654369c491c9675a1230cac
Sergio-DC <sgdc377@gmail.com>
2021-04-21 16:19:38 -0500

    DTOs migrated

---
+++ src/main/java/com/agilethought/internship/sso/dto/UpdateUserResponse.java
@@ -0,0 +1,23 @@
+package com.agilethought.internship.sso.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateUserResponse {
+
+	//The Response will be a 200 HttpStatus.OK with the following Response Body
+	private String id;
+
+	private Integer type;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String email;
+
+	private Integer status;
+
+}
