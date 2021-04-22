22a231f6f9230774f90cffaf791a1bbf38fb0eaa
RamOwen99 <owen.ramirez@agilethought.com>
2021-04-21 23:46:25 -0500

    Use validation from UserValidator and Log4j2

--- src/main/java/com/agilethought/internship/sso/services/ServiceApplicationimpl.java
+++ src/main/java/com/agilethought/internship/sso/services/ServiceApplicationimpl.java
@@ -1,23 +1,25 @@
 package com.agilethought.internship.sso.services;
 
+import com.agilethought.internship.sso.validator.user.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import lombok.extern.slf4j.Slf4j;
+
 import com.agilethought.internship.sso.domain.UserDTO;
 import com.agilethought.internship.sso.exception.BadRequestException;
 import com.agilethought.internship.sso.mapper.UserTransformer;
 import com.agilethought.internship.sso.model.User;
 import com.agilethought.internship.sso.model.UserId;
 import com.agilethought.internship.sso.repository.RepositoryApplication;
-import com.agilethought.internship.sso.exception.errorMessages.HttpExceptionMessage;
-import com.agilethought.internship.sso.exception.errorMessages.PathErrorMessage;
+import static com.agilethought.internship.sso.exception.errorMessages.ErrorMessageCreateUser.ALREADY_EXISTING_EMAIL;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import java.util.List;
 
 @Service
-@Slf4j
 public class ServiceApplicationimpl implements ServiceApplication {
+	private static final Logger logger = LogManager.getLogger();
 
 	@Autowired
 	private UserTransformer userTransformer;
@@ -28,42 +30,51 @@ public class ServiceApplicationimpl implements ServiceApplication {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 
+	@Autowired
+	private UserValidator userValidator;
+	
+
 	@Override
 	public UserId createUser(UserDTO userDTO) {
 		UserId userId = new UserId();
-
-		BusinessValidations.validate(userDTO);
-		userDTO = PopulateFields.populate(userDTO);
-
-		if (repositoryApplication.existsByEmail(userDTO.getEmail().toLowerCase().trim())) {
-			throw new BadRequestException(HttpExceptionMessage.BAD_REQUEST_MAIL_ALREADY_EXISTS,
-					PathErrorMessage.PATH_API, HttpStatus.BAD_REQUEST);
-		}
-
 		User user = userTransformer.transformer(userDTO);
-		log.info("ServiceApplicationimpl.createUser - users transformed: {}", user);
-		user.setPassword(passwordEncoder.encode(user.getPassword()));
-		user.setEmail(user.getEmail().toLowerCase().trim());
+		userValidator.validate(user);
+		if (repositoryApplication.existsByEmail(user.getEmail())) {
+			throw new BadRequestException(
+				String.format(
+					ALREADY_EXISTING_EMAIL.getErrorMessage(),
+					user.getEmail()
+				)
+			);
+		}
+		setLetterCases(user);
+		logger.info("ServiceApplicationimpl.createUser - users transformed: {}", user);
 		String userIdDb = repositoryApplication.save(user).getId();
-		log.info("ServiceApplicationimpl.createUser- User saved  successfully with id: {}", user.getId());
+		logger.info("ServiceApplicationimpl.createUser- User saved  successfully with id: {}", user.getId());
 		userId.setId(userIdDb);
-		log.info("ServiceApplicationimpl.createUser- User created successfully on mongoDB: {}", userId);
+		logger.info("ServiceApplicationimpl.createUser- User created successfully on mongoDB: {}", userId);
 		return userId;
 	}
 
 	@Override
 	public List<UserDTO> getUsers() {
-		log.info("ServiceApplicationimpl.getUsers - Before getting all the users");
+		logger.info("ServiceApplicationimpl.getUsers - Before getting all the users");
 		List<User> response = repositoryApplication.findAll();
-		log.info("ServiceApplicationimpl.getUsers -  Consulted successfully on mongoDB: {}", response);
+		logger.info("ServiceApplicationimpl.getUsers -  Consulted successfully on mongoDB: {}", response);
 		return userTransformer.listTransformer(response);
 	}
 
 	@Override
 	public List<UserDTO> getUsersByEmail(String email) {
-		log.info("ServiceApplicationimpl.getUsers - Searching users by email");
+		logger.info("ServiceApplicationimpl.getUsers - Searching users by email");
 		List<UserDTO> users = repositoryApplication.findUsersByEmail(email);
-		log.info("ServiceApplicationimpl.getUsersByEmail - getUsersByEmail operation was successful: {}", users);
+		logger.info("ServiceApplicationimpl.getUsersByEmail - getUsersByEmail operation was successful: {}", users);
 		return users;
 	}
-}// End class
+
+	private static void setLetterCases(User request){
+		request.setFirstName(request.getFirstName().toUpperCase());
+		request.setLastName(request.getLastName().toUpperCase());
+		request.setEmail(request.getEmail().toLowerCase());
+	}
+}
