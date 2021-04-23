7bd01d237c987d67d290e8ad5168763fac7dfd2c
RamOwen99 <owen.ramirez@agilethought.com>
2021-04-23 01:12:26 -0500

    Move method to validate an existing email

---
+++ src/main/java/com/agilethought/internship/sso/validator/user/CreateNewUserValidation.java
@@ -0,0 +1,35 @@
+package com.agilethought.internship.sso.validator.user;
+
+import com.agilethought.internship.sso.exception.BadRequestException;
+import com.agilethought.internship.sso.model.User;
+import com.agilethought.internship.sso.repository.RepositoryApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import static com.agilethought.internship.sso.exception.errorMessages.ErrorMessageCreateUser.ALREADY_EXISTING_EMAIL;
+
+@Service
+public class CreateNewUserValidation {
+
+    @Autowired
+    private RepositoryApplication repositoryApplication;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    public void validate(User user) {
+        this.userValidator.validate(user);
+        validateUniqueEmail(user.getEmail());
+    }
+
+    private void validateUniqueEmail(String email) {
+        if (repositoryApplication.existsByEmail(email)) {
+            throw new BadRequestException(
+                String.format(
+                    ALREADY_EXISTING_EMAIL.getErrorMessage(),
+                    email
+                )
+            );
+        }
+    }
+}
