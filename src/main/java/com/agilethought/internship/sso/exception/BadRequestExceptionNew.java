3ee7e6683da673d3e67c5f13e5857fdddb420bc6
Sergio-DC <sgdc377@gmail.com>
2021-04-27 21:27:12 -0500

    Modifying validators & exceptions to match sce-api repo

--- src/main/java/com/agilethought/internship/sso/exception/BadRequestExceptionNew.java
+++ src/main/java/com/agilethought/internship/sso/exception/BadRequestExceptionNew.java
@@ -1,11 +1,22 @@
 package com.agilethought.internship.sso.exception;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import com.agilethought.internship.sso.exception.GlobalExceptionBody.ErrorDetails;
+
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BadRequestExceptionNew extends RuntimeException{
-    public BadRequestExceptionNew(String message) {
+	private List<ErrorDetails> errorDetails;
+	
+	public BadRequestExceptionNew(String message) {
         super(message);
     }
+	
+	 public BadRequestExceptionNew(String message, List<ErrorDetails> details) {
+	    	super(message);
+	    	errorDetails = details;
+	 }
 }
