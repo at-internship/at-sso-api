6beb9472dbeebdb2762017afcb777ef3919702b2
Dayana Llerenas <dayana.llerenas@agilethought.com>
2021-05-08 00:42:32 -0500

    Refactor Details

--- src/test/java/com/agilethought/internship/sso/validator/user/NewUserValidatorTest.java
+++ src/test/java/com/agilethought/internship/sso/validator/user/NewUserValidatorTest.java
@@ -13,58 +13,46 @@ import org.mockito.MockitoAnnotations;
 
 public class NewUserValidatorTest {
 
-    @Mock
-    private RepositoryApplication repositoryApplication;
+	@Mock
+	private RepositoryApplication repositoryApplication;
 
-    @Mock
-    private UserDataValidator userDataValidator;
+	@Mock
+	private UserDataValidator userDataValidator;
 
-    @InjectMocks
-    private NewUserValidator newUserValidator;
+	@InjectMocks
+	private NewUserValidator newUserValidator;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-    @Test
-    public void testCreateUserWithEmailAlreadyTaken() {
+	@Test
+	public void testCreateUserWithEmailAlreadyTaken() {
 
-        User mockCreateUser = new User();
-        mockCreateUser.setEmail("email_already_taken@example.com");
+		User mockCreateUser = new User();
+		mockCreateUser.setEmail("email_already_taken@example.com");
 
-        doNothing().when(userDataValidator).validate(any());
-        when(repositoryApplication.existsByEmail(anyString())).thenReturn(true);
-        Exception thrownException = assertThrows(
-                BadRequestException.class,
-                () -> {
-                    newUserValidator.validate(mockCreateUser);
-                }
-        );
-        assertEquals(
-                String.format(
-                        "Email %s is already in use, try with another one or login.",
-                        mockCreateUser.getEmail()
-                ),
-                thrownException.getMessage()
-        );
+		doNothing().when(userDataValidator).validate(any());
+		when(repositoryApplication.existsByEmail(anyString())).thenReturn(true);
+		Exception thrownException = assertThrows(BadRequestException.class, () -> {
+			newUserValidator.validate(mockCreateUser);
+		});
+		assertEquals(
+				String.format("email %s is already in use, try with another one or login.", mockCreateUser.getEmail()),
+				thrownException.getMessage());
+	}
 
-    }
+	@Test
+	public void testCreateUserWithNewEmail() {
 
-    @Test
-    public void testCreateUserWithNewEmail() {
+		User mockCreateUser = new User();
+		mockCreateUser.setEmail("new_unique_email@example.com");
 
-        User mockCreateUser = new User();
-        mockCreateUser.setEmail("new_unique_email@example.com");
-
-        doNothing().when(userDataValidator).validate(any());
-        when(repositoryApplication.existsByEmail(anyString())).thenReturn(false);
-        assertDoesNotThrow(
-                () -> {
-                    newUserValidator.validate(mockCreateUser);
-                }
-        );
-
-    }
-
+		doNothing().when(userDataValidator).validate(any());
+		when(repositoryApplication.existsByEmail(anyString())).thenReturn(false);
+		assertDoesNotThrow(() -> {
+			newUserValidator.validate(mockCreateUser);
+		});
+	}
 }
--- src/test/java/com/agilethought/internship/sso/validator/user/UpdateUserValidatorTest.java
+++ src/test/java/com/agilethought/internship/sso/validator/user/UpdateUserValidatorTest.java
@@ -21,129 +21,99 @@ import static org.mockito.Mockito.when;
 
 public class UpdateUserValidatorTest {
 
-    @Mock
-    private RepositoryApplication repositoryApplication;
+	@Mock
+	private RepositoryApplication repositoryApplication;
 
-    @Mock
-    private UserDataValidator userDataValidator;
+	@Mock
+	private UserDataValidator userDataValidator;
 
-    @InjectMocks
-    private UpdateUserValidator updateUserValidator;
+	@InjectMocks
+	private UpdateUserValidator updateUserValidator;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-    }
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
 
-    @Test
-    public void testUpdateUserByIdWithMissingId() {
+	@Test
+	public void testUpdateUserByIdWithMissingId() {
 
-        User mockUpdateUser = new User();
+		User mockUpdateUser = new User();
 
-        Exception thrownException = assertThrows(
-                BadRequestException.class,
-                () -> {
-                    updateUserValidator.validate(mockUpdateUser);
-                }
-        );
-        assertEquals(
-                "Required field Id is missing.",
-                thrownException.getMessage()
-        );
+		Exception thrownException = assertThrows(BadRequestException.class, () -> {
+			updateUserValidator.validate(mockUpdateUser);
+		});
+		assertEquals("Required field id is missing.", thrownException.getMessage());
+	}
 
-    }
+	@Test
+	public void testUpdateUserByIdWithIdNotFound() {
 
-    @Test
-    public void testUpdateUserByIdWithIdNotFound() {
+		User mockUpdateUser = new User();
+		mockUpdateUser.setId("mockedId");
 
-        User mockUpdateUser = new User();
-        mockUpdateUser.setId("mockedId");
+		when(repositoryApplication.findById(anyString())).thenReturn(Optional.empty());
+		Exception thrownException = assertThrows(NotFoundException.class, () -> {
+			updateUserValidator.validate(mockUpdateUser);
+		});
+		assertEquals("User was not found with the given id: mockedId", thrownException.getMessage());
+	}
 
-        when(repositoryApplication.findById(anyString()))
-                .thenReturn(Optional.empty());
-        Exception thrownException = assertThrows(
-                NotFoundException.class,
-                () -> {
-                    updateUserValidator.validate(mockUpdateUser);
-                }
-        );
-        assertEquals(
-                "User was not found with the given id: mockedId",
-                thrownException.getMessage()
-        );
+	@Test
+	public void testUpdateUserByIdWithEmailAlreadyTaken() {
 
-    }
+		// A user with email "current_email@example.com" tries
+		// to update its email to "my_email@example.com", but
+		// it's already taken by another user.
+		User mockUpdateRequest = new User();
+		mockUpdateRequest.setId("mockedUserToUpdateId");
+		mockUpdateRequest.setEmail("my_email@example.com");
 
-    @Test
-    public void testUpdateUserByIdWithEmailAlreadyTaken() {
+		User mockOwnerOfEmail = new User();
+		mockOwnerOfEmail.setId("mockedEmailOwnerId");
+		mockOwnerOfEmail.setEmail("my_email@example.com");
 
-        // A user with email "current_email@example.com" tries
-        // to update its email to "my_email@example.com", but
-        // it's already taken by another user.
-        User mockUpdateRequest = new User();
-        mockUpdateRequest.setId("mockedUserToUpdateId");
-        mockUpdateRequest.setEmail("my_email@example.com");
+		User mockUserToUpdate = new User();
+		mockUserToUpdate.setId("mockedUserToUpdateId");
+		mockUserToUpdate.setEmail("current_email@example.com");
 
-        User mockOwnerOfEmail = new User();
-        mockOwnerOfEmail.setId("mockedEmailOwnerId");
-        mockOwnerOfEmail.setEmail("my_email@example.com");
+		when(repositoryApplication.findById("mockedUserToUpdateId")).thenReturn(Optional.of(mockUserToUpdate));
+		doNothing().when(userDataValidator).validate(any());
+		when(repositoryApplication.existsByEmail(anyString())).thenReturn(true);
+		Exception thrownException = assertThrows(BadRequestException.class, () -> {
+			updateUserValidator.validate(mockUpdateRequest);
+		});
+		assertEquals("email my_email@example.com is already in use, try with another one or login.",
+				thrownException.getMessage());
+	}
 
-        User mockUserToUpdate = new User();
-        mockUserToUpdate.setId("mockedUserToUpdateId");
-        mockUserToUpdate.setEmail("current_email@example.com");
+	@Test
+	public void testUpdateUserByIdSuccessfullyWithNewEmail() {
 
-        when(repositoryApplication.findById("mockedUserToUpdateId"))
-                .thenReturn(Optional.of(mockUserToUpdate));
-        doNothing().when(userDataValidator).validate(any());
-        when(repositoryApplication.existsByEmail(anyString())).thenReturn(true);
-        Exception thrownException = assertThrows(
-                BadRequestException.class,
-                () -> {
-                    updateUserValidator.validate(mockUpdateRequest);
-                }
-        );
-        assertEquals(
-                "Email my_email@example.com is already in use, try with another one or login.",
-                thrownException.getMessage()
-        );
+		User mockUpdateUser = new User();
+		mockUpdateUser.setId("1234");
+		mockUpdateUser.setEmail("new_unique_email@example.com");
 
-    }
+		when(repositoryApplication.findById(anyString())).thenReturn(Optional.of(mockUpdateUser));
+		doNothing().when(userDataValidator).validate(any());
+		when(repositoryApplication.existsByEmail(anyString())).thenReturn(false);
+		assertDoesNotThrow(() -> {
+			updateUserValidator.validate(mockUpdateUser);
+		});
+	}
 
-    @Test
-    public void testUpdateUserByIdSuccessfullyWithNewEmail() {
+	@Test
+	public void testUpdateUserByIdWithSameEmail() {
 
-        User mockUpdateUser = new User();
-        mockUpdateUser.setId("1234");
-        mockUpdateUser.setEmail("new_unique_email@example.com");
+		User mockUpdateUser = new User();
+		mockUpdateUser.setId("1234");
+		mockUpdateUser.setEmail("my_same_email@example.com");
 
-        when(repositoryApplication.findById(anyString()))
-                .thenReturn(Optional.of(mockUpdateUser));
-        doNothing().when(userDataValidator).validate(any());
-        when(repositoryApplication.existsByEmail(anyString())).thenReturn(false);
-        assertDoesNotThrow(
-                () -> {
-                    updateUserValidator.validate(mockUpdateUser);
-                }
-        );
-
-    }
-
-    @Test
-    public void testUpdateUserByIdWithSameEmail() {
-
-        User mockUpdateUser = new User();
-        mockUpdateUser.setId("1234");
-        mockUpdateUser.setEmail("my_same_email@example.com");
-
-        when(repositoryApplication.findById(anyString()))
-                .thenReturn(Optional.of(mockUpdateUser));
-        doNothing().when(userDataValidator).validate(any());
-        when(repositoryApplication.existsByEmail(anyString())).thenReturn(true);
-        assertDoesNotThrow(
-                () -> {
-                    updateUserValidator.validate(mockUpdateUser);
-                }
-        );
-    }
-
+		when(repositoryApplication.findById(anyString())).thenReturn(Optional.of(mockUpdateUser));
+		doNothing().when(userDataValidator).validate(any());
+		when(repositoryApplication.existsByEmail(anyString())).thenReturn(true);
+		assertDoesNotThrow(() -> {
+			updateUserValidator.validate(mockUpdateUser);
+		});
+	}
 }
