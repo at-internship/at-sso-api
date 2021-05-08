925b1681689533a9c8643de74c34b3dd260678bc
Dayana Llerenas <dayana.llerenas@agilethought.com>
2021-05-08 00:42:32 -0500

    Refactor Details

--- src/main/java/com/agilethought/internship/sso/exception/errorhandling/ErrorMessage.java
+++ src/main/java/com/agilethought/internship/sso/exception/errorhandling/ErrorMessage.java
@@ -3,17 +3,17 @@ package com.agilethought.internship.sso.exception.errorhandling;
 public class ErrorMessage {
 
 	public static final String MISSING_REQUIRED_INPUT = "Required field %s is missing.";
-	public static final String ID = "Id";
+	public static final String ID = "id";
 
 	public static final String INVALID_INPUT = "Invalid input on field %s. Correct format is: %s";
 	public static final String CORRECT_FORMAT_NUMERIC = "A number with or " +
 			"without a decimal point.";
 
-	public static final String EMAIL = "Email";
-	public static final String ALREADY_EXISTING_EMAIL = "Email %s is already in use, " +
+	public static final String EMAIL = "email";
+	public static final String ALREADY_EXISTING_EMAIL = "email %s is already in use, " +
 			"try with another one or login.";
 
-	public static final String PASSWORD = "Password";
+	public static final String PASSWORD = "password";
 
 	public static final String NOT_FOUND_RESOURCE = "%s was not found with the given id: %s";
 
@@ -26,15 +26,15 @@ public class ErrorMessage {
 
 	public static final String BAD_REQUEST_JSON = "Malformed JSON request.";
 
-	public static final String BAD_REQUEST_EMPTY_NULL_NAME = "Name is required.";
-	public static final String BAD_REQUEST_EMPTY_NULL_FIRST_NAME = "First Name is required.";
-	public static final String BAD_REQUEST_EMPTY_NULL_LAST_NAME = "Last Name is required.";
-	public static final String BAD_REQUEST_EMPTY_NULL_PASSWORD = "Password is required.";
+	public static final String BAD_REQUEST_EMPTY_NULL_NAME = "name is required.";
+	public static final String BAD_REQUEST_EMPTY_NULL_FIRST_NAME = "firstName is required.";
+	public static final String BAD_REQUEST_EMPTY_NULL_LAST_NAME = "lastName is required.";
+	public static final String BAD_REQUEST_EMPTY_NULL_PASSWORD = "password is required.";
 	public static final String BAD_REQUEST_INVALID_STATUS = "Invalid status. Field should be 0 or 1.";
-	public static final String BAD_REQUEST_EMPTY_NULL_STATUS = "Status is required.";
-	public static final String BAD_REQUEST_EMPTY_NULL_MAIL = "Mail is required.";
-	public static final String BAD_REQUEST_FORMAT_MAIL = "Format required: {Name}@{domain].{com,es}.";
-	public static final String BAD_REQUEST_MAIL_ALREADY_EXISTS = "Mail already exists.";
+	public static final String BAD_REQUEST_EMPTY_NULL_STATUS = "status is required.";
+	public static final String BAD_REQUEST_EMPTY_NULL_MAIL = "email is required.";
+	public static final String BAD_REQUEST_FORMAT_MAIL = "Format required: {name}@{domain].{com,es}.";
+	public static final String BAD_REQUEST_MAIL_ALREADY_EXISTS = "email already exists.";
 	public static final String FILTER_CHARACTERS = "Characters 'Space' and 'Line Break' are invalids";
 
 	private ErrorMessage() {
