ec1af186d729df0cd4cf436a0aaa5bcf44376f48
Ale <Ale@DESKTOP-VH89K98>
2021-05-19 14:41:44 -0500

    Added requested changes regarding security

--- src/main/java/com/agilethought/internship/sso/config/SecurityConfiguration.java
+++ src/main/java/com/agilethought/internship/sso/config/SecurityConfiguration.java
@@ -1,12 +1,15 @@
 package com.agilethought.internship.sso.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,12 +20,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	private static final String TOKEN_CREATION_ENDPOINT = "/api/v1/login";
 
+	@Value("${sso.client.id}")
+	private String clientId;
+
+	@Value("${sso.client.secret}")
+	private String clientSecret;
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf()
 				.disable()
 				.authorizeRequests()
-				.antMatchers(TOKEN_CREATION_ENDPOINT).permitAll();
+				.antMatchers(TOKEN_CREATION_ENDPOINT).permitAll()
+				.antMatchers("/api/v1/users/**").authenticated()
+		        .antMatchers("/api/v1/tokens/**").authenticated()
+		        .and()
+		        .httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder passwordEncoder = passwordEncoder();
+		auth.inMemoryAuthentication().withUser(clientId).password(passwordEncoder.encode(clientSecret)).roles("ADMIN");
 	}
 
 	@Bean
